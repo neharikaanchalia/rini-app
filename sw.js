29928d26f1c39294396bdab63fe50a4c5d5ece16
neharikaanchalia <neharika.anchalia@gmail.com>
2026-03-30 09:53:48 +0000

    offline app fix

--- sw.js
+++ sw.js
@@ -1,21 +1,34 @@
-const cacheName = 'rini-v1';
+const cacheName = 'rini-v3'; // Increment this (v3, v4...) every time you update
 const assets = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  'https://img.icons8.com/ios-filled/192/F06292/lotus.png'
 ];
 
-// Install the Service Worker
+// Install Event
 self.addEventListener('install', evt => {
   evt.waitUntil(
     caches.open(cacheName).then(cache => {
-      console.log('Rini app is caching files for offline use');
-      cache.addAll(assets);
+      return cache.addAll(assets);
+    })
+  );
+  self.skipWaiting(); // Forces the new service worker to become active immediately
+});
+
+// Activate Event (Cleanup old caches)
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== cacheName)
+        .map(key => caches.delete(key))
+      );
     })
   );
 });
 
-// Fetch files from cache if offline
+// Fetch Event
 self.addEventListener('fetch', evt => {
   evt.respondWith(
     caches.match(evt.request).then(cacheRes => {
